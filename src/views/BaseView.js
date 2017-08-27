86ea69817fbae1fddf06e808737e465dcdb76cc2
Ran Greenberg <rang@wix.com>
2017-08-27 22:16:33 +0300

    add base view

---
+++ src/views/BaseView.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+  Animated
+} from 'react-native';
+
+
+export default class BaseView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      opacityValue: this.props.opacityValue
+    }
+  }
+
+  componentDidMount() {
+    console.log('RANG', 'componentDidMount', this.state.opacityValue);
+
+    Animated.timing(
+      this.state.opacityValue,
+      {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true
+      }
+    ).start();
+  }
+}
